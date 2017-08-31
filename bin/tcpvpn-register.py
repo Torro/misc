9aeda2d606c4ab59104ef117a40d9d23959f6004
Torro <Torro@tuta.io>
2017-08-31 15:59:55 +0200

    Initial commit of tcpvpn-register.py.

---
+++ bin/tcpvpn-register.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# A quick and simple tcpvpn.com registration script
+import requests
+import random
+import string
+from bs4 import BeautifulSoup
+
+
+def gen_random():
+    ''' Generate a "cryptographically secure" pseudo-random string of
+        12 alphanumeric characters.'''
+    return ''.join(random.SystemRandom().choice(
+        string.ascii_letters + string.digits) for rangeObject in range(12))
+
+
+mainUrl = 'https://www.tcpvpn.com'
+formUrl = mainUrl + '/create-vpn-account-europe'
+parameters = {
+              'username': gen_random(),
+              'password': gen_random(),
+              'create': '',
+              'serverid': 'YOUR PREFERRED SERVERID HERE'
+             }
+
+if parameters['serverid'].isdigit() and len(parameters['serverid']) == 2:
+    pass
+else:
+    raise ValueError("Parameter 'serverid' must be two digits.")
+
+cookieJar = requests.get(mainUrl).cookies
+page = requests.post(formUrl, cookies=cookieJar, data=parameters)
+soup = BeautifulSoup(page.text, 'html.parser')
+
+username = soup.find('span', id='copyTarget').text
+password = soup.find('span', id='copyTarget2').text
+
+print(username, password, sep='\n')
